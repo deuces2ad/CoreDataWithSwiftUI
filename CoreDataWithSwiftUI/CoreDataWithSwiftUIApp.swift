3de16a226466125cc02c1bd31eee18ca91c5c6e1
Abhishek Dhiman <abhishekdhiman1408@gmail.com>
2022-06-30 13:15:08 +0530

    Initial Commit

---
+++ CoreDataWithSwiftUI/CoreDataWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWithSwiftUIApp.swift
+//  CoreDataWithSwiftUI
+//
+//  Created by abhishek dhiman on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
